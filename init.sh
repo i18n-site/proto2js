c2ab699bcab69c41c47b70646cc46ea809de38b8
i18n <i18n.site@gmail.com>
2025-09-30 15:16:58 +0800

    ♨

--- init.sh
+++ init.sh
@@ -13,11 +13,9 @@ if [ ! -d "protoc-gen-mbt" ]; then
 git clone --depth=1 git@github.com:moonbitlang/protoc-gen-mbt.git
 fi
 
-cd protoc-gen-mbt
-moonup install lastest
-moonup default lastest
-moon build -C cli
-cd ..
-
 moonup install nightly
 moonup default nightly
+
+cd protoc-gen-mbt
+moon build -C cli
+cd ..
